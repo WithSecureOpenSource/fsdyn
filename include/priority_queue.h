7540342d644ba45e61c0898410c4ee41ba5cc810
Marko Rauhamaa <marko.rauhamaa@f-secure.com>
2020-05-25 20:51:24 +0300

    Introduce priorq_t

---
+++ include/priority_queue.h
@@ -0,0 +1,68 @@
+#ifndef __FSDYN_PRIORITY_QUEUE__
+#define __FSDYN_PRIORITY_QUEUE__
+
+#include <stddef.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Priority queues (heaps) for C. */
+
+typedef struct priorq priorq_t;
+
+/* Create a priority queue.
+ *
+ * The element comparator cmp() compares the relative priority of two
+ * elements. A negative value indicates that the first arguments has a
+ * higher priority than the second argument. A zero value indicates an
+ * equal priority.
+ *
+ * The optional reloc() function is called on an element whenever the
+ * element is placed in a new location in the heap structure, which
+ * can happen any time the priority queue is operated on. The function
+ * should store the value of loc so it can be used in a subsequent
+ * priorq_remove() call.
+ *
+ * The reloc() function can also be NULL, in which case
+ * priorq_remove() cannot be used but elements can only be retrieved
+ * in the strict priority order. */
+priorq_t *make_priority_queue(int (*cmp)(const void *elem1, const void *elem2),
+                              void (*reloc)(const void *elem, void *loc));
+
+/* Destroy the priority queue structure. The elements are not affected. */
+void destroy_priority_queue(priorq_t *prq);
+
+/* Enter an element into the priority queue. One element can be
+ * enqueued twice, and two separate elements can have an equal
+ * priority. */
+void priorq_enqueue(priorq_t *prq, const void *value);
+
+/* Return true if and only if the priority queue has no elements. */
+bool priorq_empty(priorq_t *prq);
+
+/* Return the number of elements in the priority queue. */
+size_t priorq_size(priorq_t *prq);
+
+/* Remove the element with the highest priority from the priority
+ * queue and return it. The element that compares the least in terms
+ * of cmp() is considered highest priority.
+ *
+ * If the priority queue is empty, NULL is returned. */
+const void *priorq_dequeue(priorq_t *prq);
+
+/* Remove some element from the priority queue and return it. The
+ * function may execute faster than priorq_dequeue().
+ *
+ * If the priority queue is empty, NULL is returned. */
+const void *priorq_pop(priorq_t *prq);
+
+/* Remove the element with the given locator and return it. */
+const void *priorq_remove(priorq_t *prq, void *loc);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ test/priorq_test.c
@@ -0,0 +1,124 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <fsdyn/priority_queue.h>
+#include <fsdyn/avltree.h>
+
+enum {
+    K = 20,
+    N = 1000000
+};
+
+typedef struct {
+    uint8_t key[K];
+    void *loc;
+} element_t;
+
+static element_t elements[N];
+
+static void prepare_data(void)
+{
+    int i;
+    for (i = 0; i < N; i++) {
+        int j;
+        for (j = 0; j < K; j++)
+            elements[i].key[j] = random() % 256;
+    }
+}
+
+static int cmp(const void *value1, const void *value2)
+{
+    return memcmp(((element_t *) value1)->key, ((element_t *) value2)->key, K);
+}
+
+static void reloc(const void *value, void *loc)
+{
+    ((element_t *) value)->loc = loc;
+}
+
+static avl_tree_t *enter_avl_data()
+{
+    avl_tree_t *tree = make_avl_tree(cmp);
+    int i;
+    for (i = 0; i < N; i++)
+        avl_tree_put(tree, &elements[i], &elements[i]);
+    return tree;
+}
+
+static priorq_t *enter_pr_data()
+{
+    priorq_t *prq = make_priority_queue(cmp, reloc);
+    int i;
+    for (i = 0; i < N; i++)
+        priorq_enqueue(prq, &elements[i]);
+    return prq;
+}
+
+static bool test_correctness()
+{
+    fprintf(stderr, "enter_data\n");
+    avl_tree_t *tree = enter_avl_data();
+    avl_elem_t *ae = avl_tree_get_first(tree);
+    priorq_t *prq = enter_pr_data();
+    while (!priorq_empty(prq)) {
+        element_t *e = (element_t *) priorq_dequeue(prq);
+        if (e != avl_elem_get_value(ae)) {
+            fprintf(stderr, "Mismatch!\n");
+            return false;
+        }
+        ae = avl_tree_next(ae);
+    }
+    destroy_priority_queue(prq);
+    destroy_avl_tree(tree);
+    return true;
+}
+
+uint64_t now_ns()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (uint64_t) t.tv_sec * 1000000000 + t.tv_usec * 1000;
+}
+
+static void test_performance()
+{
+    fprintf(stderr, "measure AVL tree\n");
+    uint64_t start = now_ns();
+    avl_tree_t *tree = enter_avl_data();
+    avl_elem_t *ae = avl_tree_get_first(tree);
+    while (ae)
+        ae = avl_tree_next(ae);
+    destroy_avl_tree(tree);
+    uint64_t finish = now_ns();
+    fprintf(stderr, "  %g s\n", (finish - start) * 1e-9);
+    fprintf(stderr, "measure priority queue dequeue\n");
+    start = now_ns();
+    priorq_t *prq = enter_pr_data();
+    while (!priorq_empty(prq))
+        priorq_dequeue(prq);
+    destroy_priority_queue(prq);
+    finish = now_ns();
+    fprintf(stderr, "  %g s\n", (finish - start) * 1e-9);
+    fprintf(stderr, "measure priority queue remove\n");
+    start = now_ns();
+    prq = enter_pr_data();
+    int i;
+    for (i = 0; i < N; i++)
+        priorq_remove(prq, elements[i].loc);
+    destroy_priority_queue(prq);
+    finish = now_ns();
+    fprintf(stderr, "  %g s\n", (finish - start) * 1e-9);
+}
+
+int main()
+{
+    fprintf(stderr, "prepare_data\n");
+    prepare_data();
+    if (!test_correctness())
+        return EXIT_FAILURE;
+    test_performance();
+    return EXIT_SUCCESS;
+}
