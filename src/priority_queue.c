93d692ae9a633c9328d4368879ee7e706a9c6d45
Marko Rauhamaa <marko.rauhamaa@f-secure.com>
2020-05-25 20:51:24 +0300

    Introduce priorq_t

---
+++ src/priority_queue.c
@@ -0,0 +1,119 @@
+#include <stdint.h>
+#include "priority_queue.h"
+#include "fsalloc.h"
+#include "avltree_version.h"
+
+struct priorq {
+    int (*cmp)(const void *, const void *);
+    void (*reloc)(const void *, void *loc);
+    const void **storage;
+    size_t capacity;
+};
+
+static void dummy_reloc(const void *value, void *loc)
+{
+}
+
+priorq_t *make_priority_queue(int (*cmp)(const void *, const void *),
+                              void (*reloc)(const void *, void *loc))
+{
+    priorq_t *prq = fsalloc(sizeof *prq);
+    prq->cmp = cmp;
+    prq->reloc = reloc ? reloc : dummy_reloc;
+    prq->storage = NULL;
+    prq->capacity = 0;
+    return prq;
+}
+
+void destroy_priority_queue(priorq_t *prq)
+{
+    fsfree(prq->storage);
+    fsfree(prq);
+}
+
+static void assign(priorq_t *prq, size_t slot, const void *value)
+{
+    prq->storage[slot] = value;
+    prq->reloc(value, (void *) (intptr_t) slot);
+}
+
+static size_t raise(priorq_t *prq, size_t slot, const void *value)
+{
+    while (slot) {
+        size_t parent = (slot - 1) / 2;
+        if (prq->cmp(value, prq->storage[parent]) >= 0)
+            break;
+        assign(prq, slot, prq->storage[parent]);
+        slot = parent;
+    }
+    return slot;
+}
+
+void priorq_enqueue(priorq_t *prq, const void *value)
+{
+    size_t slot = prq->capacity++;
+    prq->storage =
+        fsrealloc(prq->storage, prq->capacity * sizeof *prq->storage);
+    assign(prq, raise(prq, slot, value), value);
+}
+
+size_t priorq_size(priorq_t *prq)
+{
+    return prq->capacity;
+}
+
+bool priorq_empty(priorq_t *prq)
+{
+    return priorq_size(prq) == 0;
+}
+
+static void lower(priorq_t *prq, size_t slot, const void *value)
+{
+    for (;;) {
+        size_t left = slot * 2 + 1;
+        if (left >= prq->capacity)
+            break;
+        size_t branch;
+        size_t right = left + 1;
+        if (right >= prq->capacity ||
+            prq->cmp(prq->storage[left], prq->storage[right]) < 0)
+            branch = left;
+        else branch = right;
+        if (prq->cmp(value, prq->storage[branch]) <= 0)
+            break;
+        assign(prq, slot, prq->storage[branch]);
+        slot = branch;
+    }
+    assign(prq, slot, value);
+}
+
+const void *priorq_dequeue(priorq_t *prq)
+{
+    switch (prq->capacity) {
+        case 0:
+            return NULL;
+        case 1:
+            return prq->storage[--prq->capacity];
+        default:
+            ;
+    }
+    const void *value = prq->storage[0];
+    lower(prq, 0, prq->storage[--prq->capacity]);
+    return value;
+}
+
+const void *priorq_pop(priorq_t *prq)
+{
+    if (priorq_empty(prq))
+        return NULL;
+    return prq->storage[--prq->capacity];
+}
+
+const void *priorq_remove(priorq_t *prq, void *loc)
+{
+    size_t slot = (intptr_t) loc;
+    const void *value = prq->storage[slot];
+    const void *other = prq->storage[--prq->capacity];
+    lower(prq, raise(prq, slot, other), other);
+    return value;
+}
