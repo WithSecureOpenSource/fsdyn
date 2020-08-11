60eba1088c031fe79cffb8d881890a81de9a9aa0
Marko Rauhamaa <marko.rauhamaa@f-secure.com>
2020-08-11 14:51:13 +0300

    Reimplement (and fix) Unicode canonical composition

--- src/recomposition.py
+++ src/recomposition.py
@@ -3,7 +3,13 @@
 import sys
 
 def main():
-    decompositions = {}
+    recompositions = {}
+    exclusions = set()
+    for line in open(sys.argv[2]):
+        line = line.strip()
+        if not line or line.startswith("#"):
+            continue
+        exclusions.add(int(line.split()[0], 16))
     for line in open(sys.argv[1]):
         fields = line.split(";")
         hex_cp = fields[0]
@@ -16,85 +22,45 @@ def main():
         decomposition = [ int(dc, 16) for dc in raw_decomposition.split() ]
         if len(decomposition) < 2:
             continue
-        cursor = decompositions
-        for dc in decomposition:
-            if dc not in cursor:
-                cursor[dc] = {}
-            cursor = cursor[dc]
-        cursor[None] = codepoint
+        starter, cc = decomposition
+        if codepoint not in exclusions:
+            if cc not in recompositions:
+                recompositions[cc] = {}
+            recompositions[cc][starter] = codepoint
     sys.stdout.write("""#include "charstr.h"
 
 """)
-    compose_root(decompositions)
+    emit_recompositions(recompositions)
 
-def compose_root(decompositions):
-    prefix = ["recompose"]
-    items = sorted(decompositions.items())
-    for key, value in items:
-        if key is not None:
-            prefix.append("%x" % key)
-            compose(value, prefix)
-            prefix.pop()
-    sys.stdout.write("""const char *
-_charstr_unicode_compose(const char *s, const char *end, int *codepoint)
-{
-    const char *t = charstr_decode_utf8_codepoint(s, end, codepoint);
-    if (!t)
-        return NULL;
-    for (;;) {
-        int candidate = *codepoint;
-        const char *u;
-        switch (*codepoint) {
-""")
-    for key, value in items:
-        sys.stdout.write("""            case 0x%x:
-                u = %s_%x(t, end, codepoint);
-                break;
-""" % (key, "_".join(map(str, prefix)), key))
-    sys.stdout.write("""            default:
-                u = NULL;
-        }
-        if (!u) {
-            *codepoint = candidate;
-            return t;
-        }
-        t = u; 
+def emit_recompositions(recompositions):
+    for cc in sorted(recompositions):
+        sys.stdout.write("""
+static int recompose_{}(int starter)
+{{
+    switch (starter) {{
+""".format(cc))
+        for starter, codepoint in sorted(recompositions[cc].items()):
+            sys.stdout.write("""        case {}:
+            return {};
+""".format(starter, codepoint))
+        sys.stdout.write("""        default:
+            return -1;
     }
 }
 """)
-
-def compose(decompositions, prefix):
-    items = sorted(decompositions.items())
-    for key, value in items:
-        if key is not None:
-            prefix.append("%x" % key)
-            compose(value, prefix)
-            prefix.pop()
-    sys.stdout.write("""static const char *
-%s(const char *s, const char *end, int *codepoint)
+    sys.stdout.write("""
+int _charstr_unicode_primary_composite(int starter, int cc)
 {
-""" % "_".join(map(str, prefix)))
-    if None in decompositions:
-        sys.stdout.write("""    *codepoint = 0x%x;
-    return s;
-}
-
-""" % decompositions[None])
-        return
-    sys.stdout.write("""    const char *t = charstr_decode_utf8_codepoint(s, end, codepoint);
-    if (!t)
-        return NULL;
-    switch (*codepoint) {
+    switch (cc) {
 """)
-    for key, value in items:
-        sys.stdout.write("""        case 0x%x:
-            return %s_%x(t, end, codepoint);
-""" % (key, "_".join(map(str, prefix)), key))
+    for cc in sorted(recompositions):
+        sys.stdout.write("""        case {}:
+            return recompose_{}(starter);
+""".format(cc, cc))
     sys.stdout.write("""        default:
-            return NULL;
+            return -1;
     }
 }
-
 """)
 
 if __name__ == '__main__':
