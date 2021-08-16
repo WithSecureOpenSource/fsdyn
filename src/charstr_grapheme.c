035fc7e5b3d53a34a9c043e2390bbe58e2ded64a
Marko Rauhamaa <marko.rauhamaa@f-secure.com>
2021-08-16 16:40:41 +0300

    Add charstr_skip_utf8_grapheme()

---
+++ src/charstr_grapheme.c
@@ -0,0 +1,121 @@
+#include "charstr.h"
+
+/* https://unicode.org/reports/tr29/#Grapheme_Cluster_Boundaries */
+
+static const char *skip_trailer(const char *s1,
+                                const char *s2,
+                                int cp1,
+                                const char *end)
+{
+    while (charset_unicode_grapheme_break_prop_is_extend(cp1) ||
+           charset_unicode_grapheme_break_prop_is_zwj(cp1) ||
+           charset_unicode_grapheme_break_prop_is_sm(cp1)) {
+        if (s2 == end)
+            return s2;
+        s1 = s2;
+        s2 = charstr_decode_utf8_codepoint(s1, end, &cp1);
+        if (!s2)
+            return NULL;
+    }
+    return s1;
+}
+
+static const char *skip_emoji_modifier_sequence(const char *s1,
+                                                const char *s2,
+                                                int cp1,
+                                                const char *end)
+{
+    for (;;) {
+        while (charset_unicode_grapheme_break_prop_is_extend(cp1)) {
+            if (s2 == end)
+                return s2;
+            s1 = s2;
+            s2 = charstr_decode_utf8_codepoint(s1, end, &cp1);
+            if (!s2)
+                return NULL;
+        }
+        if (!charset_unicode_grapheme_break_prop_is_zwj(cp1))
+            return skip_trailer(s1, s2, cp1, end);
+        if (s2 == end)
+            return s2;
+        s1 = s2;
+        s2 = charstr_decode_utf8_codepoint(s1, end, &cp1);
+        if (!s2)
+            return NULL;
+        if (!charset_unicode_emoji_prop_is_extended_pictographic(cp1))
+            return skip_trailer(s1, s2, cp1, end);
+        if (s2 == end)
+            return s2;
+        s1 = s2;
+        s2 = charstr_decode_utf8_codepoint(s1, end, &cp1);
+        if (!s2)
+            return NULL;
+    }
+}
+
+static const char *skip_emoji_flag_sequence(const char *s1,
+                                            const char *s2,
+                                            int cp1,
+                                            const char *end)
+{
+    if (!charset_unicode_grapheme_break_prop_is_ri(cp1))
+        return skip_trailer(s1, s2, cp1, end);
+    if (s2 == end)
+        return s2;
+    s1 = charstr_decode_utf8_codepoint(s2, end, &cp1);
+    if (!s1)
+        return NULL;
+    return skip_trailer(s2, s1, cp1, end);
+}
+
+const char *charstr_skip_utf8_grapheme(const char *s, const char *end)
+{
+    int cp0, cp1;
+    const char *s1 = charstr_decode_utf8_codepoint(s, end, &cp0);
+    if (!s1)
+        return NULL;
+    if (s1 == end)
+        return s1;
+    const char *s2 = charstr_decode_utf8_codepoint(s1, end, &cp1);
+    if (!s2)
+        return NULL;
+    if (charset_unicode_emoji_prop_is_extended_pictographic(cp0))
+        return skip_emoji_modifier_sequence(s1, s2, cp1, end);
+    if (charset_unicode_grapheme_break_prop_is_ri(cp0))
+        return skip_emoji_flag_sequence(s1, s2, cp1, end);
+    for (;;) {
+        if (cp0 == '\r' && cp1 == '\n')
+            return s2;
+        if (cp0 == '\r' || cp0 == '\n' ||
+            charset_unicode_grapheme_break_prop_is_control(cp0))
+            return s1;
+        if (cp1 == '\r' || cp1 == '\n' ||
+            charset_unicode_grapheme_break_prop_is_control(cp1))
+            return s1;
+        if ((!charset_unicode_grapheme_break_prop_is_l(cp0) ||
+             (!charset_unicode_grapheme_break_prop_is_l(cp1) &&
+              !charset_unicode_grapheme_break_prop_is_v(cp1) &&
+              !charset_unicode_grapheme_break_prop_is_lv(cp1) &&
+              !charset_unicode_grapheme_break_prop_is_lvt(cp1))) &&
+            ((!charset_unicode_grapheme_break_prop_is_lv(cp0) &&
+              !charset_unicode_grapheme_break_prop_is_v(cp0)) ||
+             (!charset_unicode_grapheme_break_prop_is_v(cp1) &&
+              !charset_unicode_grapheme_break_prop_is_t(cp1))) &&
+            ((!charset_unicode_grapheme_break_prop_is_lvt(cp0) &&
+              !charset_unicode_grapheme_break_prop_is_t(cp0)) ||
+             !charset_unicode_grapheme_break_prop_is_t(cp1)) &&
+            (!charset_unicode_grapheme_break_prop_is_extend(cp1) &&
+             !charset_unicode_grapheme_break_prop_is_zwj(cp1)) &&
+            !charset_unicode_grapheme_break_prop_is_sm(cp1) &&
+            !charset_unicode_grapheme_break_prop_is_prepend(cp0))
+            break;
+        cp0 = cp1;
+        s1 = s2;
+        if (s1 == end)
+            return s1;
+        s2 = charstr_decode_utf8_codepoint(s1, end, &cp1);
+        if (!s2)
+            return NULL;
+    }
+    return s1;
+}
---
+++ test/charstr_grapheme_test.c
@@ -0,0 +1,105 @@
+#if _POSIX_C_SOURCE < 200809L
+#define _GNU_SOURCE
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <fsdyn/charstr.h>
+#include <fsdyn/list.h>
+#include <fsdyn/integer.h>
+
+static char *add_codepoint(const char *begin, const char *end,
+                           char *outp, char *outendp)
+{
+    if (!begin)
+        return outp;
+    int cp = 0;
+    while (begin != end) {
+        unsigned digit = charstr_digit_value(*begin++);
+        assert(digit != -1);
+        cp = cp * 16 + digit;
+    }
+    return charstr_encode_utf8_codepoint(cp, outp, outendp);
+}
+
+static bool fail()
+{
+    return false;
+}
+
+static bool test(const char *testcase)
+{
+    const char *end = testcase + strlen(testcase);
+    const char *tp = testcase;
+    const char *begin_value = NULL;
+    char outbuf[500];
+    char *outp = outbuf;
+    char *outendp = outbuf + sizeof outbuf - 1;
+    list_t *offsets = make_list();
+    while (*tp) {
+        int cp;
+        const char *nextp = charstr_decode_utf8_codepoint(tp, end, &cp);
+        assert(nextp);
+        switch (cp) {
+            case '\t':
+            case ' ':
+            case 0xd7: /* × */
+                outp = add_codepoint(begin_value, tp, outp, outendp);
+                begin_value = NULL;
+                break;
+            case 0xf7: /* ÷ */
+                outp = add_codepoint(begin_value, tp, outp, outendp);
+                begin_value = NULL;
+                if (outp != outbuf)
+                    list_append(offsets, as_integer(outp - outbuf));
+                break;
+            default:
+                assert(cp < 256);
+                if (!begin_value)
+                    begin_value = tp;
+        }
+        tp = nextp;
+    }
+    outp = add_codepoint(begin_value, tp, outp, outendp);
+    assert(outp);
+    *outp = '\0';
+    bool ok = true;
+    if (outp != outbuf) {
+        const char *p = outbuf;
+        while (p != outp) {
+            p = charstr_skip_utf8_grapheme(p, outp);
+            if (!p || list_empty(offsets) ||
+                as_intptr(list_pop_first(offsets)) != p - outbuf) {
+                ok = fail();
+                break;
+            }
+        }
+        if (!list_empty(offsets))
+            ok = fail();
+    }
+    destroy_list(offsets);
+    return ok;
+}
+
+int main(int argc, const char *const *argv)
+{
+    FILE *f = fopen(argv[1], "r");
+    char *line = NULL;
+    size_t length = 0;
+    bool ok = true;
+    for (int ln = 1; ok && getline(&line, &length, f) > 0; ln++) {
+        list_t *parts = charstr_split(line, '#', 1);
+        const char *body = list_elem_get_value(list_get_first(parts));
+        ok = test(body);
+        list_foreach(parts, (void *) fsfree, NULL);
+        destroy_list(parts);
+    }
+    free(line);
+    fclose(f);
+    if (!ok)
+        return EXIT_FAILURE;
+    fprintf(stderr, "Ok\n");
+    return EXIT_SUCCESS;
+}
